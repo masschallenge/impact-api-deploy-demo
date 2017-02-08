2171cd7c98119e40e81f177e77ffb7200ceefdaf
Shankar Ambady <shanbady@gmail.com>
2017-02-08 15:51:04 -0500

    adding prod compose file and removing mysql dependency

--- web/scripts/start.sh
+++ web/scripts/start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 cd /wwwroot
-echo "waiting on mysql"
-while ! mysqladmin ping -h"mysql" -u --silent 2>/dev/null; do
-	sleep 3
-	echo "..."
-done
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 gunicorn --log-file=- --workers 4 --name impact_gunicorn -b 0.0.0.0:8000 impact.wsgi:application
