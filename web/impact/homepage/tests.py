57d74bcb93df5f9bbda119a9a6716904180f7661
Nathan Wilson <nathan@masschallenge.org>
2017-02-09 23:30:24 +0000

    Added failing test

--- web/impact/homepage/tests.py
+++ web/impact/homepage/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 
 class TestHomepage(TestCase):
 
+    def test_fail(self):
+        assert(False)
+
     def test_homepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
